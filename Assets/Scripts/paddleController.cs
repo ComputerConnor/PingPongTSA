89e835dd7c2919881c7a3d828b106371b20ebd95
Manas0807 <mmalla0807@gmail.com>
2020-03-06 22:02:57 -0600

    Added Base Ping Pong Game

---
+++ Assets/Scripts/paddleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddleController : MonoBehaviour
+{
+    public Transform AIPaddle;
+    float force = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            Vector3 dir = AIPaddle.position - transform.position;
+            other.GetComponent<Rigidbody>().velocity = dir.normalized * force + new Vector3(0, 4, 0);
+        }
+    }
+
+}
